ff6f518f1989e91eca9525458dfc3e978d24a2f7
chlguswl <chjguswl@naver.com>
2025-10-27 18:23:01 +0900

    10.27 18:22 user 브랜치테스트

--- teamProject/src/main/java/com/example/teamProject/user/controller/UserController.java
+++ teamProject/src/main/java/com/example/teamProject/user/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.example.teamProject.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
 	// 테스트 (1~2)
+	
+	@RequestMapping("/user/list.do")
+	public String area(Model model) throws Exception{
+
+        return "/user/default"; // 기본 확장자 파일을 jsp로 잡아놨기에 확장자 생략
+    }
 }
