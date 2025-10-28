c1917a39ec44113158adaeb2caeeec2fa36ee470
dragonstudy9 <a01074021766@gmail.com>
2025-10-28 18:19:21 +0900

    10/28 로그인, 회원가입 진행 중

--- teamProject/src/main/java/com/example/teamProject/user/controller/UserController.java
+++ teamProject/src/main/java/com/example/teamProject/user/controller/UserController.java
@@ -1,11 +1,60 @@
 package com.example.teamProject.user.controller;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.teamProject.user.dao.UserService;
+import com.google.gson.Gson;
 
 @Controller
 public class UserController {
-	// 테스트 (1~2)
-	//second test
+	
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping("/user/login.do")
+	public String login(Model model) throws Exception {
+		
+		return "/user/login";
+	}
+	
+	@RequestMapping("/user/join.do")
+	public String join(Model model) throws Exception {
+		
+		return "/user/join";
+	}
+	
+	@RequestMapping(value = "/user/login.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String login(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = userService.login(map);
+
+		return new Gson().toJson(resultMap);
+	}
+	
+	@RequestMapping(value = "/user/join.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String join(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = userService.addUser(map);
+
+		return new Gson().toJson(resultMap);
+	}
+	
+	@RequestMapping(value = "/user/check.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String check(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = userService.userIdCheck(map);
+
+		return new Gson().toJson(resultMap);
+	}
 }
