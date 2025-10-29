d9f49a4641e6f2cf32d0e95618eb65cb5a73e53d
chlguswl <chjguswl@naver.com>
2025-10-29 13:20:55 +0900

    10.29 13:20 main 브랜치 업뎃

--- teamProject/src/main/java/com/example/teamProject/main/model/Main.java
+++ teamProject/src/main/java/com/example/teamProject/main/model/Main.java
@@ -2,4 +2,22 @@ package com.example.teamProject.main.model;
 
 public class Main {
 	// Main 모델
+	
+	// 사용자 정보
+	private String userId;
+	
+	// 상품 정보
+	private int proNo;
+	private String proName;
+	private String storeName;
+	private int price;
+	private int deliveryFee;
+	private int cnt;
+	private int sellCount;
+	private String createdAt; // 상품등록일 
+	
+	// 판매자 채팅 가능여부 (Y/N)
+	private String chatEnabled;
+	
+	
 }
