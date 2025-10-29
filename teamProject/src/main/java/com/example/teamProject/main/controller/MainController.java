4aab959fd6f76cf22914b5f94e946e7b02725728
KIMYESEUL <kimesil1008@ajou.ac.kr>
2025-10-29 17:47:44 +0900

    2025.10.29 메인화면 전체 수정

--- teamProject/src/main/java/com/example/teamProject/main/controller/MainController.java
+++ teamProject/src/main/java/com/example/teamProject/main/controller/MainController.java
@@ -1,16 +1,24 @@
 package com.example.teamProject.main.controller;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.teamProject.main.dao.MainService;
+import com.google.gson.Gson;
 
 @Controller
 public class MainController {	
 	
-	//@Autowired	
-	//@RequestMapping	
-	//@ResponseBody
-	
+	@Autowired
+	MainService MainService;
+
 	@RequestMapping("/main.do") 
     public String main(Model model) throws Exception{
 
@@ -22,5 +30,13 @@ public class MainController {
 
         return "/main/header";
     }
+
+	@RequestMapping(value = "/main/list.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String proList(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = MainService.getProList(map);
+		return new Gson().toJson(resultMap);
+	}
 	
 }
