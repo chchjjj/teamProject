66e44ac7f8580f3f4db33bc4ff5fe3e13f8bf074
KIMYESEUL <kimesil1008@ajou.ac.kr>
2025-10-30 16:16:35 +0900

    Merge branch 'develop' of https://github.com/chchjjj/teamProject.git into main 프로덕트

--- teamProject/src/main/java/com/example/teamProject/product/controller/ProductController.java
+++ teamProject/src/main/java/com/example/teamProject/product/controller/ProductController.java
@@ -1,15 +1,57 @@
 package com.example.teamProject.product.controller;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.teamProject.product.dao.ProductService;
+import com.google.gson.Gson;
 
 @Controller
 public class ProductController {
 
+	@Autowired
+	ProductService ProductService;
+	
 	@RequestMapping("/productDetail.do") 
-    public String productDetail(Model model) throws Exception{
-
+    public String productDetail(@RequestParam("proNo") int proNo, Model model) throws Exception{
+		model.addAttribute("proNo", proNo);
         return "/product/productDetail";
     }
+	
+	@RequestMapping("/product/cart.do") 
+    public String cart(Model model) throws Exception{
+
+        return "/product/cart";
+    }
+	
+	@RequestMapping(value = "/product/info.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String info(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = ProductService.getProInfo(map);
+		return new Gson().toJson(resultMap);
+	}
+	
+	@RequestMapping(value = "/product/TopOptlist.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String TopOptlist(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = ProductService.getTopOptList(map);
+		return new Gson().toJson(resultMap);
+	}
+	
+	@RequestMapping(value = "/product/AllOptlist.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String AllOptlist(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = ProductService.getAllOptList(map);
+		return new Gson().toJson(resultMap);
+	}
 }
--- teamProject/src/main/java/com/example/teamProject/product/model/Product.java
+++ teamProject/src/main/java/com/example/teamProject/product/model/Product.java
@@ -5,4 +5,28 @@ import lombok.Data;
 @Data
 public class Product {
 
+		// 상품 정보
+		private int proNo;
+		private String proName;
+		private String storeName;
+		private String proInfo;
+		private int price; 
+		private int deliveryFee;	
+		private int cnt;
+		private int sellCount;
+		private String createdAt; // 상품등록일 
+		private String lettering;
+		
+		// 판매자 채팅 가능여부 (Y/N)
+		private String isChatEnabled;
+		
+		// 상위옵션
+		private int topOptionId;
+		private String optionName;
+		private String isQuantitySelectAble;
+		
+		// 하위옵션
+		private String subOptionId;
+		private String valueName;
+		private String priceDiff;
 }
