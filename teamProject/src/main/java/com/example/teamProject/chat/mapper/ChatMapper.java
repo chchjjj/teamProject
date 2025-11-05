3228c2a6eb6180842e2b9b19ee39db11e372694b
chlguswl <chjguswl@naver.com>
2025-11-05 16:27:06 +0900

    11.05 16:26 chat 브랜치 (pom.xml 수정 등)

--- teamProject/src/main/java/com/example/teamProject/chat/mapper/ChatMapper.java
+++ teamProject/src/main/java/com/example/teamProject/chat/mapper/ChatMapper.java
@@ -2,7 +2,12 @@ package com.example.teamProject.chat.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.teamProject.chat.model.Chat;
+
 @Mapper
 public interface ChatMapper {
-
+	
+	// 구매자 발송메세지 저장
+	void insertBuyerChatMsg(Chat message);
+	
 }
