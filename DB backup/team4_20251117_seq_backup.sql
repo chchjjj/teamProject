6d318e27467c9ca12d28328de9be167f8cecef70
HYUN JI <chjguswl@naver.com>
2025-11-17 18:37:58 +0900

    Add files via upload

---
+++ DB backup/team4_20251117_seq_backup.sql
@@ -0,0 +1,158 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-11월-17-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence CHAT_MSG_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."CHAT_MSG_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 247 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CHAT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."CHAT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 78 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence FEE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."FEE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence MEMBERSHIP_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."MEMBERSHIP_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 11 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PAYMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PAYMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 280 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUCT_OPTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PRODUCT_OPTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 128 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REVIEW_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REVIEW_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 42 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SELLER_IMG_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SELLER_IMG_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 31 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SELLER_INFO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SELLER_INFO_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 66 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_AD_HISTORY_TBL
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_AD_HISTORY_TBL"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_AD_TBL
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_AD_TBL"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 101 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CART_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_CART_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 480 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CART_OPT_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_CART_OPT_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1267 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_DELI_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_DELI_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 308 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_GRADE_CODE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_GRADE_CODE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_INGREDIENT_TBL
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_INGREDIENT_TBL"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 221 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_ORDER_DETAIL_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_ORDER_DETAIL_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 956 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_ORDER_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_ORDER_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 978 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_ORDER_OPTION_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_ORDER_OPTION_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 2592 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_PICK_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_PICK_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 325 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_PRO_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_PRO_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 123 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_PRODUCT_IMG_NUM
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_PRODUCT_IMG_NUM"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 100 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_QA_TBL
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_QA_TBL"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_QUESTION_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_QUESTION_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 20 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_SELLER_IMG_NUM
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_SELLER_IMG_NUM"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 7 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_STORE_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_STORE_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 5 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_SUB_OPTION_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_SUB_OPTION_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 59 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_SYSTEM_IMAGE_TBL
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_SYSTEM_IMAGE_TBL"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_TOP_OPTION_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_TOP_OPTION_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 19 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_USER_ADDRESS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_USER_ADDRESS"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 101 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SUB_OPTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SUB_OPTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 148 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
